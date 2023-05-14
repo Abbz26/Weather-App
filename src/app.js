e00f40825c54419538fd89393d9b75acb7f1478e
Abbz26 <Abbegalecover26@gmail.com>
2023-05-14 17:09:38 -0500

    adjusted the api

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
 let apiKey = "33d07e1a28d76a09d64977t04bafo00c";
 let apiUrl =
-  "https://api.shecodes.io/weather/v1/forecast?query=NewYork&${apiKey}&units=metric";
+  "https://api.shecodes.io/weather/v1/forecast?query=Lisbon&key=33d07e1a28d76a09d64977t04bafo00c&units=metric";
+console.log(apiUrl);
