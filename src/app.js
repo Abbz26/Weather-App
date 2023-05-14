3bd79bd5894374bf6379488ec02cd37c97b7e359
Abbz26 <Abbegalecover26@gmail.com>
2023-05-14 17:30:08 -0500

    add more information to the js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 function displayTemperature(response) {
-  console.log(response.data);
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement = innerHTML = response.data.main.temp;
 }
 
 let apiKey = "33d07e1a28d76a09d64977t04bafo00c";
