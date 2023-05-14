e5a2d10e2a4b64f852245ad45e909e24c7157caf
Abbz26 <Abbegalecover26@gmail.com>
2023-05-14 18:33:39 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -100,7 +100,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiKey = "8895b49556a931615f57525343f9ed37";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
