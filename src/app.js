494e07e788e7e1625f8df852fbe40a8291a0b6ca
Abbz26 <Abbegalecover26@gmail.com>
2023-05-14 17:17:55 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
+
 let apiKey = "33d07e1a28d76a09d64977t04bafo00c";
 let apiUrl =
   "https://api.shecodes.io/weather/v1/forecast?query=Lisbon&key=33d07e1a28d76a09d64977t04bafo00c&units=metric";
+
 console.log(apiUrl);
+axios.get(url).then(displayTemperature);
