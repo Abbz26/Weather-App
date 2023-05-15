4ea9b0f4e2610b1759ad737a9ad89f33adc2514d
Abbz26 <Abbegalecover26@gmail.com>
2023-05-15 14:12:15 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -103,7 +103,7 @@ function displayTemperature(response) {
 function search(query) {
   let apiKey = "33d07e1a28d76a09d64977t04bafo00c";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&appid=${apiKey}&units=metric`;
-  https: axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
