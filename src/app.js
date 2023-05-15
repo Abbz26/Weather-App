234432e84f46ad6e1f38b982b58039e778c6c3af
Abbz26 <Abbegalecover26@gmail.com>
2023-05-14 19:10:42 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -69,7 +69,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "8895b49556a931615f57525343f9ed37";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
