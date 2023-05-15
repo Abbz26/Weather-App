42871953666488ceca163e54043d856f0527d9e2
Abbz26 <Abbegalecover26@gmail.com>
2023-05-14 19:04:22 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -68,8 +68,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "3d07e1a28d76a09d64977t04bafo00c";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiKey = "8895b49556a931615f57525343f9ed37";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -100,8 +100,8 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "33d07e1a28d76a09d64977t04bafo00c";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&appid=${apiKey}&units=metric`;
+  let apiKey = "8895b49556a931615f57525343f9ed37";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
