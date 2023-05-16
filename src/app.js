80d54555ead3950011f6561ee1ff9f7df5e60155
Abbz26 <Abbegalecover26@gmail.com>
2023-05-15 20:19:21 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ function displayTemperature(response) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = response.data.main.humidity;
+  humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
