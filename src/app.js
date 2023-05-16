96618608fe7c05270a27a9057b748590cba24208
Abbz26 <Abbegalecover26@gmail.com>
2023-05-15 21:34:12 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -76,7 +76,7 @@ function displayCelsiusTemperature(event) {
   fahrenheitLink.classList.remove("active");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-console.log(celsiusTemperature);
+console.log(celsius);
 
 let celsiusTemperature = null;
 
