a2c3fe2936fee3fc83aa8dba8768eeb08e2edbf0
Abbz26 <Abbegalecover26@gmail.com>
2023-05-15 21:49:59 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -34,6 +34,7 @@ function displayTemperature(response) {
   celsiusTemperature = response.data.temperature.current;
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  console.log(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
@@ -76,7 +77,7 @@ function displayCelsiusTemperature(event) {
   fahrenheitLink.classList.remove("active");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-console.log(response.data.temperature.current);
+
 let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
