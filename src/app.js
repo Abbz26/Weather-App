a65c6601eb73b5d94f913675bc68899b9bcde1e8
Abbz26 <Abbegalecover26@gmail.com>
2023-05-18 18:34:43 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ function formatDate(timestamp) {
 }
 
 function displayforecast(response) {
-  console.log(response.data.daily);
+  console.log(response.data.day);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
