1e1382170ad6a1036679c3da9b1d4b64d65fe3bb
Abbz26 <Abbegalecover26@gmail.com>
2023-05-18 19:48:17 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ function displayforecast(response) {
       `<div class="col-2">
                 <div class="weather-forecast-date">${forecastDay.dt}</div>
                 <img
-                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecast.daily.condition.icon}.png"
+                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.daily.condition.icon}.png"
                   alt=""
                   width="42"
                 />
